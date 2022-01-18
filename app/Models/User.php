24dc1e0e3ad92d3cd78917f4d0fd813228dfbb97
ellastyko <vadimsergeev1337@gmail.com>
2022-01-18 12:31:07 +0200

    auto clear token

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+//    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -41,4 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /*
+        User Methods
+    */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\{
+    AuthController,
+    UserController,
+    PostController,
+    CategoryController,
+    CommentController
+};
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,3 +20,80 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('auth')->group(function() {
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('/password-reset/{token}', [AuthController::class, 'passwordReset']);
+});
+
+
+Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'users'], function () {
+
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class,'show']);
+    Route::post('', [UserController::class, 'store']);
+    Route::post('/avatar', [UserController::class, 'avatar']);
+    Route::patch('{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+
+Route::prefix('posts')->group(function () {
+
+    Route::get('', [PostController::class, 'index']);
+    Route::get('/{post_id}', [PostController::class, 'show']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('', [PostController::class, 'store']);
+        Route::patch('/{post_id}', [PostController::class, 'update']);
+        Route::delete('/{post_id}', [PostController::class, 'destroy']);
+    });
+
+    // Categories
+    Route::get('/{post_id}/categories', [PostController::class, 'showCategories']);
+
+    // Comments
+    Route::get('/{post_id}/comments', [PostController::class, 'showComments']);
+    Route::middleware('auth:sanctum')->post('/{post_id}/comments', [PostController::class, 'storeComment']);
+
+    // Likes
+    Route::get('/{post_id}/like', [PostController::class, 'show_likes']);
+    Route::middleware('auth:sanctum')->post('{post_id}/like', [PostController::class, 'storeLike']);
+    Route::middleware('auth:sanctum')->delete('{post_id}/like', [PostController::class, 'destroyLike']);
+});
+
+
+
+Route::prefix('categories')->group( function () {
+
+
+    Route::get('', [CategoryController::class, 'index']);
+    Route::get('/{category_id}', [CategoryController::class, 'show']);
+    Route::get('/{category_id}/posts', [CategoryController::class, 'showPostsByCategory']);
+
+    Route::middleware(['auth:sanctum', 'can:store,update'])->group(function () {
+
+        Route::post('', [CategoryController::class, 'store']);
+        Route::patch('{id}', [CategoryController::class, 'update']);
+        Route::delete('/{category_id}', [CategoryController::class, 'destroy']);
+    });
+});
+
+
+Route::group([
+    'prefix' => 'comments',
+    'middleware' => 'auth:sanctum'
+], function () {
+
+    Route::get('{comment_id}', [CommentController::class, 'show'])->withoutMiddleware('auth:sanctum');
+    Route::patch('{comment_id}', [CommentController::class, 'update']);
+    Route::delete('{comment_id}', [CommentController::class, 'destroy']);
+
+    Route::get('{comment_id}/like', [CommentController::class, 'show_likes'])->withoutMiddleware('auth:sanctum');
+    Route::post('{comment_id}/like', [CommentController::class, 'store_like']);
+    Route::delete('{comment_id}/like', [CommentController::class, 'destroy_like']);
+});
